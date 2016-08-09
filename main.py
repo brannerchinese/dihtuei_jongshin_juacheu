34523419d74273ead22c817be04a58390d99ac5e
David Branner <brannerchinese@gmail.com>
2016-08-09 19:41:44 -0400

    Break out more methods in main.py

--- main.py
+++ main.py
@@ -9,16 +9,31 @@ from urllib.parse import urlunparse
 import config
 
 class Login():
+    """Create and populate login object."""
     def __init__(self):
+        self.credentials = None
+        self.config = None
+        self.session = None
+        self.input_fields = None
+        self.private = None
 
-        # Get config and credential data.
+        # Populate attributes
+        self.configure()
+        self.create_session()
+        self.log_in()
+
+    def configure(self):
+        """Get config and credential data."""
         configurations = config.Configurations()
-        credentials = configurations.credentials
+        self.credentials = configurations.credentials
         self.config = configurations.config
 
-        # Create Requests session for cookie persistence.
+    def create_session(self):
+        """Create Requests session for cookie persistence."""
         self.session = requests.Session()
 
+    def log_in(self):
+        """Get login page, find input fields, post login data."""
         # Get login page.
         url = urlunparse(
                 (self.config['scheme'], self.config['base_url'],
@@ -27,13 +42,13 @@ class Login():
                 )
         response = self.session.get(url)        # r.status_code = 200
 
-        # Find form input fields and update with email and password.
+        # Find form's input fields and update with email and password.
         root = html.document_fromstring(response.content)
         form = root.body.forms[0]
         self.input_fields = {item.name: item.value for item in form
                                         if item.tag == 'input'}
-        self.input_fields.update({'email': credentials['email'],
-                             'password': credentials['password']})
+        self.input_fields.update({'email': self.credentials['email'],
+                             'password': self.credentials['password']})
 
         # Post log-in data; special endpoint /sessions is used for this.
         url = urlunparse(
@@ -44,8 +59,8 @@ class Login():
         self.private = self.session.post(url, data=self.input_fields)
 
     def fetch_all_paths(self):
+        """Fetch all known paths."""
         return [self.fetch_path(path) for path in self.config['paths']]
-
 
     def fetch_path(self, path):
         """Log into desired path"""
