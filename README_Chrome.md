b4f3226748dbd0eab7be014dd8f7f82a20e237cd
David Branner <brannerchinese@gmail.com>
2016-09-21 21:55:59 -0400

    Add new Chrome files and update some READMEs

--- README_Chrome.md
+++ README_Chrome.md
@@ -1,9 +1,6 @@
-## PhantomJS
+## Chrome
 
- 1. Install `phantomjs` binary. 
- 
-    * [Download](http://phantomjs.org/download.html) the package and copy the binary `bin/phantomjs` into current project directory.
-    * The path of this binary is passed to `webdriver.PhantomJS`, so if the binary is placed elsewhere, the code must reflect that.
+Running with `webdriver.Chrome` requires installing ChromeDriver (done with Homebrew). See https://sites.google.com/a/chromium.org/chromedriver/getting-started on `chromedriver`.
 
  1. Sample code derived from [Techstonia blog](http://techstonia.com/scraping-with-phantomjs-and-python.html):
  
@@ -11,9 +8,7 @@
     from bs4 import BeautifulSoup
     from selenium import webdriver
     
-    PHANTOMJS_PATH = './phantomjs'
-    
-    browser = webdriver.PhantomJS(PHANTOMJS_PATH)
+    browser = webdriver.Chrome()
     
     # Test a dynamic site
     browser.get(
@@ -26,6 +21,9 @@
     # Test Google's home page, standard test
     browser.get('https://google.com')
     print(browser.title)
+
+    # Finish.
+    browser.close()
     ```
     
     Both tests work correctly and no visible browser opens. Tried code `companies_selenium_authentication_manual.py`.
---
+++ utils_selenium_authentication.py
@@ -0,0 +1,132 @@
+#!/usr/bin/python
+# selenium_authentication.py
+# David Branner
+
+import io
+import lxml
+import lxml.html
+import requests
+from selenium import webdriver
+from urllib.parse import urlunparse
+
+import config
+
+class Login():
+    """Create and populate login object."""
+    def __init__(self, debug=False, endpoint=''):
+        self.browser = webdriver.Chrome()
+        self.config = None
+        self.credentials = None
+        self.debug_print = DebugPrinter(debug=debug).debug_print
+        if debug:
+            print('\nDebugging on.')
+        self.input_fields = None
+        self.private = None
+        self.session = None
+
+        # Populate attributes
+        self.configure()
+        self.log_in()
+        self.get_endpoint(endpoint)
+
+    def configure(self):
+        """Get config and credential data."""
+        configurations = config.Configurations()
+        self.credentials = configurations.credentials
+        self.debug_print(self.credentials) # qqq
+        self.config = configurations.config
+        self.debug_print(self.config) # qqq
+
+    def log_in(self):
+        """Get login page, find input fields, post login data."""
+
+        # Get login page.
+        self.get_endpoint(endpoint=self.config['paths']['login'])
+
+        # Post log-in data.
+        email_form = self.browser.find_element_by_xpath("//input[@id='email']")
+        pw_form = self.browser.find_element_by_xpath("//input[@id='password']")
+        email_form.send_keys(self.credentials['email'])
+        pw_form.send_keys(self.credentials['password'])
+
+        # Initial log-in returns /private endpoint.
+        self.browser.find_element_by_xpath("//input[@type='submit']").click()
+
+    def get_endpoint(self, endpoint=''):
+        url = urlunparse(
+                (self.config['scheme'], self.config['base_url'], endpoint,
+                '', '', '')
+                )
+        self.browser.get(url)
+
+    def change_browsers(self):
+        """Transfer webdriver.PhantomJS cookies to webdriver.Chrome"""
+        self.browser.get_cookies()
+        pass
+
+
+class DebugPrinter():
+    def __init__(self, debug=False):
+        self.debug = debug
+
+    def debug_print(self, *content):
+        """Print statement that only prints if self.debug is True."""
+        if self.debug:
+            print(*content)
+
+
+class Cleaner():
+
+    def __init__(self, browser, debug=False):
+        self.browser = browser
+        self.debug_print = DebugPrinter(debug=debug).debug_print
+        self.parser = None
+        self.tree = None
+        self.undesirables = [
+                '//script',
+                "//meta[@name=\'csrf-token\']", # or just get rid of all meta?
+                "//link[@rel=\'stylesheet\']",
+                ]
+
+        self.make_parser()
+        self.find_root()
+        self.remove_undesirable_elements()
+    
+    def find_root(self):
+        self.tree = lxml.etree.parse(
+                io.StringIO(self.browser.page_source), self.parser)
+
+        # Clean
+        before_removing = len(list(self.tree.iter()))
+        self.remove_undesirable_elements()
+        after_removing = len(list(self.tree.iter()))
+        self.debug_print('\nRemoved {} elements'.
+                format(before_removing-after_removing))
+
+    def remove_undesirable_elements(self):
+        """If an 'undesirable' element is in document, remove and report."""
+        for undesirable in self.undesirables:
+            need_to_remove = any(item in self.tree.iter()
+                            for item in self.tree.xpath(undesirable))
+            if need_to_remove:
+                self.debug_print('\nAny {} elements present?'.
+                        format(undesirable), need_to_remove
+                     )
+                for element in self.tree.xpath(undesirable):
+                    self.debug_print(' * acting now on {}'.
+                            format(element))
+                    element.getparent().remove(element)
+                self.debug_print('All {} elements now removed?'.
+                        format(undesirable),
+                        all(item not in self.tree.iter()
+                                for item in self.tree.xpath(undesirable))
+                     )
+
+    def make_parser(self):
+        """Instantiate parser"""
+        self.parser = lxml.etree.HTMLParser(
+                recover=True,
+                remove_comments=True, # important bec. of random-length comment
+                no_network=True,
+                remove_blank_text=True)
+
